a952fd404e732b461f084e2fc8f8dc1d2808a3a3
Owen Leather <owenleather03@gmail.com>
2023-07-08 22:11:10 -0400

    added truss json import

--- truss.py
+++ truss.py
@@ -3,6 +3,8 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import sympy
+import json
+
 
 # class syntax
 class JointType(Enum):
@@ -55,12 +57,14 @@ class Link():
         self.member_force = force
 
 class Truss():
-    MAX_TENSILE = 50 # kN
-    MAX_COMPRESSIVE = 50 # kN
+    MAX_TENSILE = 9 # kN
+    MAX_COMPRESSIVE = 6 # kN
     COST_PER_JOINT = 5 # Dollars
     COST_PER_M = 15 # Dollars
+    MAX_MULTIPLIER = 3 # Triple joint
+    MINIMUM_TRUSS_LENGTH = 1
 
-    def __init__(self, joints, links, forces):
+    def __init__(self, joints = [], links = [], forces = [], filename = ""):
         '''
         @param joints: list of Joint objects
         @param links: list of tuples connecting joints by index
@@ -68,7 +72,99 @@ class Truss():
         self.joints = joints
         self.links = links
         self.forces = forces
+        if filename != "":
+            self.open_file(filename, print_data = True)
+
     
+    def open_file(self, filename, print_data=False):
+        f = open(filename)
+  
+        # returns JSON object as 
+        # a dictionary
+        data = json.load(f)
+        self.joints = []
+        self.links = []
+        self.forces = []
+        for i, node in enumerate(data["nodes"]):
+            split = node.split(',')
+            x = float(split[0])
+            y = float(split[1])
+            type = JointType.GUSSET
+            grounded = False
+            for support in data["supports"]:
+                attached = int(support)
+                if attached == i:
+                    type_str = str(data["supports"][support])
+                    if type_str == "P":
+                        type = JointType.PIN
+                        grounded = True
+                    if type_str == "Rh":
+                        type = JointType.ROLLER
+                        grounded = True
+            if print_data:
+                print("Joint",i, type, x, y, grounded)
+            self.joints.append(Joint(type, x, y, grounded=grounded))
+
+        for member in data["members"]:
+            split = member.split(',')
+            i0 = int(split[0])
+            i1 = int(split[1])
+            self.links.append(Link(i0, i1))
+            if print_data:
+                print("Link:", i0, i1)
+
+        for force in data["forces"]:
+            split = force.split(',')
+            i = int(split[0])
+            fx = float(split[1])
+            fy = float(split[2])
+            if print_data:
+                print("Force", i, fx, fy)
+        
+            self.forces.append(Force(fx=fx, fy=fy, joints_applied=[i]))
+
+        print("joints = [")
+        for joint in self.joints:
+            print("\tJoint({0}, {1}, {2}, grounded={3}),".format(("JointType.PIN" if joint.type == JointType.PIN else ("JointType.ROLLER" if joint.type == JointType.ROLLER else "JointType.GUSSET")), joint.x, joint.y, joint.grounded))
+        print("]")
+
+        print("links = [")
+        for link in self.links:
+            print("\tLink({0}, {1}),".format(link.i0, link.i1))
+        print("]")
+
+        print("forces = [")
+        for force in self.forces:
+            print("\tForce({0}, {1}, joints_applied=[{2}]),".format(force.fx, force.fy, force.joints_applied[0]))
+        print("]")
+
+        print("joints = [")
+        for i, joint in enumerate(self.joints):
+            if joint.type == JointType.PIN:
+                print("\tJoint(JointType.PIN, {0}, {1}, grounded={2}),".format(joint.x, joint.y, joint.grounded))
+            elif joint.type == JointType.ROLLER:
+                print("\tJoint(JointType.ROLLER, {0}, {1}, grounded={2}),".format(joint.x, joint.y, joint.grounded))
+            else:
+                if joint.y == 0:
+                    print("\tJoint(JointType.GUSSET, {0}, {1}, grounded={2}),".format(joint.x, joint.y, joint.grounded))
+                else:
+                    print("\tJoint(JointType.GUSSET, xs[{0}], ys[{1}], grounded={2}),".format(i, i, joint.grounded))
+        print("]")
+
+        print("x0 = [")
+        for joint in self.joints:
+            print("\t{0},".format(joint.x))
+        for joint in self.joints:
+            print("\t{0},".format(joint.y))
+        print("]")
+
+    
+    def print_joints(self):
+        print("joints = [")
+        for i, joint in enumerate(self.joints):
+            print("\tJoint({0}, {1}, {2}, grounded={3}), # {4}".format(("JointType.PIN" if joint.type == JointType.PIN else ("JointType.ROLLER" if joint.type == JointType.ROLLER else "JointType.GUSSET")), joint.x, joint.y, joint.grounded, i))
+        print("]")
+
     def solve(self, print_solution = False):
         '''
         @return cost, success : the cost of the bridge and whether the truss survives the load
@@ -133,19 +229,24 @@ class Truss():
             # Add fnet_y equation to equations list and external fy to knowns list
             equations.append(fnet_y)
             knowns.append(-joint.fey)
-            print(knowns)
+            # print(knowns)
 
         A, B = self.construct_system(equations, knowns, unknowns)
 
-        X = np.linalg.inv(A).dot(B)
-
+        X = []
+        try:
+            X = np.linalg.inv(A).dot(B)
+        except:
+            X = [[10000]]*len(unknowns)
+            print("Unsolvable")
+            
         for i, unknown in enumerate(unknowns):
             unknown_assign[unknown](X[i][0])
             if print_solution:
                 print(unknown, "=", X[i][0])
         
         # Calculate cost and success
-        success = self.check_constraints(X, unknowns)        
+        success = self.check_constraints()        
         cost = self.calculate_cost()
 
         if not success:
@@ -168,19 +269,47 @@ class Truss():
 
         return A, B
 
-    def check_constraints(self, X, unknowns):
+    def check_constraints(self):
         success = True
+        for link in self.links:
+            abs_max = 0
+
+            # Tensile
+            if link.member_force > 0:
+                abs_max = self.MAX_TENSILE
+            # Compressive
+            else:
+                abs_max = self.MAX_COMPRESSIVE
+
+            for i in range(1, self.MAX_MULTIPLIER+1):
+                link.multiplier = i
+
+                if abs(link.member_force) < abs_max * link.multiplier:
+                    break
+                elif i == self.MAX_MULTIPLIER:
+                    success = False
+
+            # Check for minimum distance of 1 m
+            j0 = self.joints[link.i0]
+            j1 = self.joints[link.i1]
+            d = math.sqrt(math.pow(j1.x-j0.x,2) + math.pow(j1.y-j0.y, 2))
+            if d < self.MINIMUM_TRUSS_LENGTH:
+                # print(d)
+                success = False
+            
+            
         return success
 
     def calculate_cost(self):
         cost = 0
-
+        total_m = 0
         # Calculate cost of trusses ($15 per m)
         for i, link in enumerate(self.links):
             j0 = self.joints[link.i0]
             j1 = self.joints[link.i1]
             d = math.sqrt(math.pow(j1.x-j0.x,2) + math.pow(j1.y-j0.y, 2))
-            cost += d * self.COST_PER_M
+            cost += d * self.COST_PER_M * link.multiplier
+            total_m += d * link.multiplier
         
         # Calculate cost of gussets ($5 per joint)
         cost += len(self.joints) * self.COST_PER_JOINT
@@ -190,8 +319,8 @@ class Truss():
     def apply_external_forces(self):
         for force in self.forces:
             if len(force.joints_applied) == 1:
-                self.joints[force.joints_applied[0]].fex = force.fex
-                self.joints[force.joints_applied[0]].fey = force.fey
+                self.joints[force.joints_applied[0]].fex = force.fx
+                self.joints[force.joints_applied[0]].fey = force.fy
             else:    
                 total_len = abs(self.joints[force.joints_applied[-1]].x - self.joints[force.joints_applied[0]].x)
                 force_per_len = float(force.fy) / total_len
@@ -207,21 +336,21 @@ class Truss():
 
     def plot(self, title = "", plot_external = False, plot_member = False):
         for link in self.links:
-            plt.plot([self.joints[link.i0].x, self.joints[link.i1].x], [self.joints[link.i0].y, self.joints[link.i1].y], color='black', marker='o')
+            plt.plot([self.joints[link.i0].x, self.joints[link.i1].x], [self.joints[link.i0].y, self.joints[link.i1].y], color='black', marker='o', linewidth=link.multiplier)
             plt.annotate('{0}'.format(link.i0), xy=(self.joints[link.i0].x, self.joints[link.i0].y), xytext=(2, 2), textcoords='offset points')
             plt.annotate('{0}'.format(link.i1), xy=(self.joints[link.i1].x, self.joints[link.i1].y), xytext=(2, 2), textcoords='offset points')
 
         for joint in self.joints:
             if((joint.fex != 0 or joint.fey != 0)) and plot_external:
                 mag = round(math.sqrt(joint.fex*joint.fex+joint.fey*joint.fey), 3)
-                plt.arrow(joint.x, joint.y, joint.fex, joint.fey, width=.5, label="force")
+                plt.arrow(joint.x, joint.y, joint.fex/6, joint.fey/6, width=.3, label="force")
                 # plt.annotate('{0}kN'.format(str(mag)), xy=(joint.x+joint.fex, joint.y+joint.fey), xytext=(0, 0), textcoords='offset points')
-                t = plt.text(joint.x+joint.fex, joint.y+joint.fey, '{0}kN'.format(mag), fontsize=10)
+                t = plt.text(joint.x+joint.fex/6, joint.y+joint.fey/6, '{0}kN'.format(mag), fontsize=10)
                 t.set_bbox(dict(facecolor='grey', alpha=0.5, edgecolor='grey'))
             if((joint.fx != 0 or joint.fy != 0)) and plot_member:
                 mag = round(math.sqrt(joint.fx*joint.fx+joint.fy*joint.fy), 3)
-                plt.arrow(joint.x, joint.y, joint.fx, joint.fy, width=.5, label="force")
-                t = plt.text(joint.x+joint.fx, joint.y+joint.fy, '{0}kN'.format(mag), fontsize=10)
+                plt.arrow(joint.x, joint.y, joint.fx/6, joint.fy/6, width=.3, label="force")
+                t = plt.text(joint.x+joint.fx/6, joint.y+joint.fy/6, '{0}kN'.format(mag), fontsize=10)
                 t.set_bbox(dict(facecolor='grey', alpha=0.5, edgecolor='grey'))
         
         if plot_member:
